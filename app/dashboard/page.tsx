20518ef5d90c351bc77b47b1c8b4579752205811
canfinancialsolutions <canfinancialsolutions@gmail.com>
2026-01-10 11:06:19 -0600

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -588,12 +588,9 @@ export default function Dashboard() {
     </div>
     <div className="px-3 py-1 bg-gray-200 text-xs font-semibold rounded text-center">Cycle Start on {cycleStart}</div>
     <div className="px-3 py-1 bg-gray-200 text-xs font-semibold rounded text-center">Cycle End on {cycleEnd}</div>
-    <div className="px-3 py-2 bg-gray-200 text-xs font-semibold rounded">Cycle Days<div className="text-center text-xs font-bosemiboldld">{cycleDays}</div>
-     </div>
-    <div className="px-3 py-2 bg-gray-200 text-xs font-semibold rounded">Today Meetings<div className="text-center text-xs font-semibold">{meetingTodayCount}</div>
-     </div>
-    <div className="px-3 py-2 bg-gray-200 text-xs font-semibold rounded">Tomorrow Meetings<div className="text-center text-xs font-semibold">{meetingTomorrowCount}</div>
-     </div>
+    <div className="px-3 py-2 bg-gray-200 text-xs font-semibold rounded text-center text-xs font-bosemiboldld">Cycle Days {cycleDays}</div>
+    <div className="px-3 py-2 bg-gray-200 text-xs font-semibold rounded text-center text-xs font-bosemiboldld">Today Meetings {meetingTodayCount}</div>
+    <div className="px-3 py-2 bg-gray-200 text-xs font-semibold rounded text-center text-xs font-bosemiboldld">Tomorrow Meetings {meetingTomorrowCount}</div>
     </div>);
   })()}
             <Button variant="secondary" onClick={toggleAllCards}>{allVisible ? "Hide All" : "Show All"}</Button> 
