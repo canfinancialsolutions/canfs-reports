5a4b9139685a37fa4a5801ccd57ddbdecf9c55c3
canfinancialsolutions <canfinancialsolutions@gmail.com>
2026-01-17 18:30:53 -0600

    Update DashboardClient.tsx

--- app/dashboard/DashboardClient.tsx
+++ app/dashboard/DashboardClient.tsx
@@ -1,7 +1,7 @@
 "use client";
 export const dynamic = "force-dynamic";
 
-import { createBrowserSupabase } from '/lib/supabaseClient'
+import { createBrowserSupabase } from '/lib/'
 
 import React, { useEffect, useMemo, useRef, useState } from "react";
 import type { CSSProperties } from "react";
